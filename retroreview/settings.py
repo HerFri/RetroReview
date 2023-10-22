f9b0c64a313611b4fcaca8a6058f65e48c1cd799
Her Fri <herbert.frichert@outlook.de>
2023-10-22 23:02:12 +0200

    add summernote, edit settings

--- retroreview/settings.py
+++ retroreview/settings.py
@@ -29,7 +29,10 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['retro-review.herokuapp.com', 'localhost','retro-review-a5a97966a295.herokuapp.com',]
+ALLOWED_HOSTS = ['retro-review.herokuapp.com',
+                  'localhost','retro-review-a5a97966a295.herokuapp.com',
+                  '127.0.0.1',
+                  ]
 
 
 # Application definition
@@ -43,6 +46,7 @@ INSTALLED_APPS = [
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
+    'django_summernote',
     'review',
 ]
 
--- review/admin.py
+++ review/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
+from .models import Game, Review
+from django_summernote.admin import SummernoteModelAdmin
 
 # Register your models here.
+@admin.register(Review)
+class ReviewAdmin (SummernoteModelAdmin):
+    
+    list_display = ('title', 'slug', 'username', 'game', 'created_on')
+    search_fields = ['title', 'game', 'content']
+    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ('status', 'created_on')   
+    summernote_fields = ('content')
+
+
+#@admin.register(Comment)
+#class CommentAdmin(admin.ModelAdmin):
+#    list_display = ('username', 'body', 'review', 'created_on', 'approved')
+#    list_filter = ('approved', 'created_on')
+#    search_fields = ('username', 'email', 'body')
+#    actions = ['approve_comments']                      #
+#                                                        #   
+#    def approve_comments(self, request, queryset):      # needed in project?
+#        queryset.update(approved=True)                  #
+
+#admin.site.register(Game)
+
+@admin.register(Game)
+class GameAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'genre', 'year', 'platform', 'rating')
+    search_fields = ['title', 'game', 'gerne', 'year', 'platform', 'rating']
+    prepopulated_fields = {'slug': ('title',)}
+ #   list_filter = ('created_on',)
